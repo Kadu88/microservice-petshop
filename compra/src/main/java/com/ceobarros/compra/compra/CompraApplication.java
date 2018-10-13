22efeb04d5ab7511d32936263f657a23443db338
Carlos Barros <carlosuntr@gmail.com>
2018-10-13 00:05:16 -0300

    first commit

---
+++ compra/src/main/java/com/ceobarros/compra/compra/CompraApplication.java
@@ -0,0 +1,12 @@
+package com.ceobarros.compra.compra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraApplication.class, args);
+	}
+}
