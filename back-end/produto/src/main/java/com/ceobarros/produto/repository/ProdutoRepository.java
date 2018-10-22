bb561f9fa8762514c3f3fafd1eda856666dc29fe
Carlos Barros <carlosuntr@gmail.com>
2018-10-22 20:22:30 -0300

    deleted files

--- back-end/produto/src/main/java/com/ceobarros/produto/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ceobarros.produto.repository;
-
-import com.ceobarros.produto.model.Produto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProdutoRepository extends CrudRepository<Produto, Long> {
-}
